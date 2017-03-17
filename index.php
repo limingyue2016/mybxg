260e3baf9875b15e3d9f3a5b3e9914d8937d3a6a
limingyue <18500353719@163.com>
2017-03-17 21:13:29 +0800

    login

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$path = 'index';
+$filename = 'index';
+
+if(array_key_exists('PATH_INFO',$_SERVER)){
+	$pathinfo = $_SERVER['PATH_INFO'];
+	$pathinfo = explode('/', substr($pathinfo, 1));
+	if(count($pathinfo) == 2) {
+		$path = $pathinfo[0];
+		$filename = $pathinfo[1];
+	}
+
+}
+else{
+        $filename = 'login';
+    }
+    include './views/' . $path . '/' . $filename . '.html';
+
