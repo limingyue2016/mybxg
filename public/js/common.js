e239548053fbe9c6096acac1a827c3601a796fc3
limingyue <18500353719@163.com>
2017-03-18 21:37:43 +0800

    退出页面完成

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,62 @@
 
-	NProgress.start();
+define(['jquery','cookie'],function($){
 
-	NProgress.done();
-
+	// 控制左侧导航菜单的显示和隐藏
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	// 没有登录的时候跳转到登录页面
+	var pathname = location.pathname;
+	var flag = $.cookie('PHPSESSID');
+	if(!flag && pathname.indexOf('login') == -1){
+		// 没有登录
+		location.href = '/index/login'
+	}
+	// 实现登录功能
+	$('#loginForm').submit(function(){
+		var formData = $(this).serialize();
+		$.ajax({
+			type : 'post',
+			url : '/api/login',
+			data : formData,
+			dataType : 'json',
+			success : function(data){
+				if(data.code == 200){
+					// console.log(data.result);
+					var logInfo = JSON.stringify(data.result);
+					// 实现cookie数据的跨页面共享
+					$.cookie('logInfo',logInfo,{path : '/'});
+					location.href = '/index/index'
+				}
+			},
+			error : function(data){
+				console.log(data.responseText);
+			}
+		});
+		return false;//阻止默认行为
+	});
+
+	// 退出功能
+	$("#logoutId").click(function () {
+		$.ajax({
+			type:"post",
+			url:"/api/logout",
+			dataType:"json",
+			success: function (data) {
+				if(data.code ==200){
+					location.href="/index/login"
+				}
+
+			}
+
+		})
+	})
+
+
+	// 渲染登录信息
+	var obj = JSON.parse($.cookie('logInfo'));
+	$('.aside .profile img').attr('src',obj.tc_avatar);
+	$('.aside .profile h4').html(obj.tc_name);
+
+});
